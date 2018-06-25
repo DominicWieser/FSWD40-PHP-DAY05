0b636ec4fc513d8dabcedce7fba833c50b13fa66
DominicWieser <dominic.wieser1995@live.at>
2018-06-25 09:29:13 +0200

    adding folders and files

---
+++ Classwork/classwork.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Classwork</title>
+	<!-- GENERAL STYLE -->
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<!-- Create a form to insert data to the database and a button, that when it is clicked, will show you the result in table using php ajax -->
+</body>
+</html>
